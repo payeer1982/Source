9c9dd0246cdabd31c3df076db6d64620e697c7cc
payeer1982 <39153086+payeer1982@users.noreply.github.com>
2018-05-28 18:55:12 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -112,7 +112,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        vSeeds.push_back(CDNSSeedData("209.250.248.103","209.250.248.103"));
+        vSeeds.push_back(CDNSSeedData("0",   "209.250.248.103"));
         //vSeeds.push_back(CDNSSeedData("1",  ""));
 		//vSeeds.push_back(CDNSSeedData("2", ""));
 		//vSeeds.push_back(CDNSSeedData("3", ""));
